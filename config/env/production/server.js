2154e29ec1f5b1d3320e9a804fdc6bdb54a06677
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2023-06-12 20:26:50 -0600

    Update code to strapi template

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,14 +1,7 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  host: "0.0.0.0",
-  port: process.env.PORT,
-  url: env('DATABASE_URL'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array("APP_KEYS"),
   },
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
-  },
-})
+});
